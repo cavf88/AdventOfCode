1558469354120e815718d1732e275db8845103e1
unknown <cavf88@hotmail.com>
2025-12-01 12:27:07 -0500

    Day 1: much simpler solution for part 1

--- Advent_of_code_2025/Day1/main.cpp
+++ Advent_of_code_2025/Day1/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    int dialVal = 50;
+    int dialPosition = 50;
     const uint16_t  DIAL_MAX_VAL = 100;
     int passwordValue = 0;
     string inputLine;
@@ -22,28 +22,41 @@ int main()
         if(dialDirection == 'L')
             lineDialVal *= -1;
 
-        const uint16_t absoluteDialVal = abs(dialVal + lineDialVal);
-        dialVal += lineDialVal;
 
-        if(dialVal < 0)
-        {
-            //uint16_t absoluteDialVal = abs(dialVal);
-            //passwordValue += absoluteDialVal / DIAL_MAX_VAL;
-            dialVal = DIAL_MAX_VAL - (absoluteDialVal % DIAL_MAX_VAL);
-        }
-        
-        if(dialVal >= DIAL_MAX_VAL)
-        {
-            //uint16_t absoluteDialVal = abs(dialVal);
-            //passwordValue += absoluteDialVal / DIAL_MAX_VAL;
-            dialVal = absoluteDialVal % DIAL_MAX_VAL;
-        }
-
-        //if(absoluteDialVal >= DIAL_MAX_VAL)
-        //    passwordValue += absoluteDialVal % DIAL_MAX_VAL;
-
-        if(dialVal == 0)
+        //passwordValue += (abs(dialPosition) + abs(lineDialVal))/ DIAL_MAX_VAL;
+        dialPosition += lineDialVal;
+        dialPosition = dialPosition % DIAL_MAX_VAL;
+        if(dialPosition == 0)
             passwordValue++;
+
+        //const uint16_t absoluteDialVal = dialVal + abs(lineDialVal);
+        //cout << "absoluteDialVal:" << absoluteDialVal << endl;
+        //dialVal += lineDialVal;
+//
+        //if(dialVal < 0)
+        //{
+        //    //uint16_t absoluteDialVal = abs(dialVal);
+        //    //passwordValue += absoluteDialVal / DIAL_MAX_VAL;
+        //    dialVal = DIAL_MAX_VAL - (absoluteDialVal % DIAL_MAX_VAL);
+        //}
+        //
+        //if(dialVal >= DIAL_MAX_VAL)
+        //{
+        //    //uint16_t absoluteDialVal = abs(dialVal);
+        //    //passwordValue += absoluteDialVal / DIAL_MAX_VAL;
+        //    dialVal = absoluteDialVal % DIAL_MAX_VAL;
+        //}
+//
+        ////if(absoluteDialVal >= DIAL_MAX_VAL)
+        ////    passwordValue += absoluteDialVal % DIAL_MAX_VAL;
+//
+        //if(dialVal == 0)
+        //    passwordValue++;
+        //else if(absoluteDialVal > DIAL_MAX_VAL)
+        //{
+        //    cout << "HERE" << endl;
+        //    passwordValue += absoluteDialVal / DIAL_MAX_VAL;
+        //}
 
 
         //if(dialVal == DIAL_MAX_VAL)
@@ -52,7 +65,7 @@ int main()
         //    passwordValue++;
         //}
         
-        cout << dialVal << endl;
+        cout << dialPosition << endl;
 
         //if(lineDialVal > dialVal)
         //    dialVal += lineDialVal;
@@ -75,20 +88,20 @@ int main()
         //   //}   
         //
         //
-        //f(dialDirection == 'R')
+        //if(dialDirection == 'R')
         //
         //   dialVal += lineDialVal;
         //
 
-        //ialVal = dialVal % DIAL_MAX_VAL;
+        //dialVal = dialVal % DIAL_MAX_VAL;
         //
-        //f(dialVal == 0)
+        //if(dialVal == 0)
         //
         //   passwordValue++;
         //
     }
 
-    cout << "The final dial value is: " << dialVal << endl;
+    cout << "The final dial value is: " << dialPosition << endl;
     cout << "The password is: " << passwordValue << endl;
 
     return 0;
