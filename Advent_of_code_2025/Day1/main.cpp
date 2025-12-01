321e91da376db576e71ac9998a9c5e6caf28da7d
unknown <cavf88@hotmail.com>
2025-12-01 10:18:45 -0500

    Day 1: test_inpurt works

--- Advent_of_code_2025/Day1/main.cpp
+++ Advent_of_code_2025/Day1/main.cpp
@@ -6,12 +6,33 @@ using namespace std;
 
 int main()
 {
+    int dialVal = 50;
+    const uint16_t  DIAL_MAX_VAL = 100;
     string inputLine;
-    fstream inputFile("input.txt");
+    fstream inputFile("test_input.txt");
     while(getline(inputFile, inputLine))
     {
         cout << inputLine << endl;
+        uint16_t lineDialVal = 0;
+        char dialDirection;
+
+        sscanf(inputLine.c_str(), "%c%d", &dialDirection, &lineDialVal);
+
+        if(dialDirection == 'L')
+        {
+            dialVal -= lineDialVal;
+            if(dialVal < 0)
+                dialVal = DIAL_MAX_VAL + dialVal;
+        }
+        
+        if(dialDirection == 'R')
+        {
+            dialVal = (dialVal + lineDialVal) % DIAL_MAX_VAL;
+        }
+
     }
-    
+
+    cout << "The final dial value is: " << dialVal << endl;
+
     return 0;
 }
