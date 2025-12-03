a2bebe31c8b4602953d7b0a7a74f4a2782a4bf96
unknown <cavf88@hotmail.com>
2025-12-03 09:35:48 -0500

    Day3: Input reading

--- Advent_of_code_2025/Day3/main.cpp
+++ Advent_of_code_2025/Day3/main.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int main()
 {
+    string inputLine;
+    fstream inputFile("test_input.txt");
+    while(getline(inputFile, inputLine))
+    {
+        cout << inputLine << endl;
+    }
     return 0;
 }
